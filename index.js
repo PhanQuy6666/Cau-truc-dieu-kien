9231561423a327bf6bc592f332a92f2af62c1bc0
Quý <quycao97@gmail.com>
2022-07-24 10:34:52 +0700

    demo

---
+++ index.js
@@ -0,0 +1,21 @@
+let diemtrungbinh = +prompt("Nhap diem trung binh cua ban: ");
+var is_correct_check = diemtrungbinh > 0;
+var is_correct_check1 = diemtrungbinh < 10;
+var is_correct_khongdat = diemtrungbinh < 5;
+var is_correct_dat = diemtrungbinh < 7;
+
+if (is_correct_check&&is_correct_check1){
+    if(is_correct_khongdat){
+        alert("Khong dat")
+    }
+    else  if(is_correct_dat) {
+        alert("Dat");
+    }
+            else{
+            alert("Tot");
+                          }              }
+    
+
+else{
+    document.getElementById("result").innerHTML="Hay tai lai trang";
+}
